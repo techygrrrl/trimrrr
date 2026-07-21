456a1289a0c4e77e092d960c639da9d28eb0ae7f
techygrrrl <techygrrrl@proton.me>
2026-07-21 01:10:28 -0400

    vertically centre

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import VideoEditor from './components/VideoEditor'
 
 function App() {
   return (
-    <VideoEditor />
+    <div className="flex flex-col h-dvh justify-center items-center">
+      <div className="w-full px-4">
+        <VideoEditor />
+      </div>
+    </div>
   )
 }
 
